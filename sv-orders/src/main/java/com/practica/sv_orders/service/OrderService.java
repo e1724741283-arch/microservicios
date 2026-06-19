24d2cb6c4cedb56ee7ecd8a5ad38a72dbea22465
CEPE1724 <edisonnacional1@hotmail.com>
2026-06-18 21:26:24 -0500

    add

--- sv-orders/src/main/java/com/practica/sv_orders/service/OrderService.java
+++ sv-orders/src/main/java/com/practica/sv_orders/service/OrderService.java
@@ -23,6 +23,40 @@ public class OrderService {
         Order order = new Order();
         order.setId(id);
         order.setDate(new Date().toString());
+        order.setDetails(dto.getDetails());
+        order.setUserId(dto.getUserId());
+        orderRepository.save(order);
+
+        redis.delete("order:all");
+        return Map.of("Mensaje", "Pedido creado con exito", "id", id);
      
+    }
+
+    public Map<String, Object> getOrders(String id){
+        String cacheKey = "order:" + id;
+        String cachedOrder = redis.opsForValue().get(cacheKey);
+        if (cachedOrder != null) {
+            try {
+                Map<String, Object> orderCache = objectMapper.readValue(
+                    cachedOrder, new TypeReference<Map<String, Object>>() {});
+                    return Map.of("fuente", "Cache Redis (2ms)", "datos", orderCache);
+            } catch (Exception e) {
+                System.out.println(e.toString());
+                redis.delete(cacheKey);
+            }
+        }
+
+        Optional<Order> orderOpt = orderRepository.findById(id);
+        if (orderOpt.isEmpty()) {
+            return Map.of("Mensaje", "Pedido no encontrado");
+        }
+        Order order = orderOpt.get();
+        try {
+            String orderJson = objectMapper.writeValueAsString(order);
+            redis.opsForValue().set(cacheKey, orderJson, 60, TimeUnit.SECONDS);
+        } catch (Exception e) {
+            System.out.println(e.toString());
+        }
+        return Map.of("fuente", "Base de datos (50ms)", "datos", order);
     }
 }
